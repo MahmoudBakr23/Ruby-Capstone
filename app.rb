19ba0cec4a1a73c6eafb5007bfd7e0158eeabd69
Mahmoud Bakr <mbakr6821@gmail.com>
2020-07-29 04:49:52 +0200

    modify the desired website to scrap

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 
 def web_scraper
-  url = "https://www.facebook.com/mahmoud.bakr.1460/"
+  url = "https://www.goodreads.com/quotes"
   document = ::OpenURI.open_uri(url)
   parsed_document = Nokogiri::HTML(document)
 end
