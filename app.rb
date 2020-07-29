efc3ec7be7a7bb149e79b4bf7089539cc84520c0
Mahmoud Bakr <mbakr6821@gmail.com>
2020-07-29 06:23:28 +0200

    modifying scraper method

--- app.rb
+++ app.rb
@@ -2,9 +2,19 @@ require 'nokogiri'
 require 'open-uri'
 
 def web_scraper
-  url = "https://www.goodreads.com/quotes"
-  document = ::OpenURI.open_uri(url)
-  parsed_document = Nokogiri::HTML(document)
-end
+  url = 'https://www.16personalities.com/personality-types'
+  doc = ::OpenURI.open_uri(url)
+  document = Nokogiri::HTML(doc)
+  characters = []
+  characters_types = document.css('a.type')
 
+  characters_types.each do |c|
+    character = {
+      name: c.css('h4').text,
+      type: c.css('h5').text,
+      describtion: c.css('div.snippet').text
+    }
+    characters.push(character)
+  end
+end
 web_scraper
