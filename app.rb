2abac530624cee52766cff28a1f88e4b2093cc1b
Mahmoud Bakr <mbakr6821@gmail.com>
2020-07-30 05:45:56 +0200

    Add class Scrap and a new method web_scraper_articles for scrapping the articles from the same url

--- app.rb
+++ app.rb
@@ -1,20 +1,48 @@
 require 'nokogiri'
 require 'open-uri'
 
-def web_scraper
-  url = 'https://www.16personalities.com/personality-types'
-  doc = ::OpenURI.open_uri(url)
-  document = Nokogiri::HTML(doc)
-  characters = []
-  characters_types = document.css('a.type')
+class Scrap
+  def initialize(url, _doc, _document)
+    @url = url
+    @doc = ::OpenURI.open_uri(url)
+    @document = Nokogiri::HTML(@doc) do |config|
+      config.strict.noblanks
+    end
+  end
 
-  characters_types.each do |c|
-    character = {
-      name: c.css('h4').text,
-      type: c.css('h5').text,
-      describtion: c.css('div.snippet').text
-    }
-    characters.push(character)
+  def web_scraper
+    characters = []
+    characters_types = @document.css('a.type')
+
+    characters_types.each do |c|
+      character = {
+        name: c.css('h4').text.strip,
+        type: c.css('h5').text.strip,
+        describtion: c.css('div.snippet').text.strip
+      }
+      characters.push(character)
+    end
+  end
+
+  def web_scraper_articles
+    articles = []
+    articles_scrapping = @document.css('article')
+
+    articles_scrapping.each do |a|
+      article = {
+        title: a.css('div.type a.with-border').text.strip,
+        headline: a.css('a.title').text.strip,
+        date: a.css('span.date').text.strip,
+        author: a.css('span.author').text.strip,
+        number_of_comments: a.css('span.comments a.with-border').text.strip
+      }
+      articles.push(article)
+    end
   end
 end
-web_scraper
+
+scrapping = Scrap.new('https://www.16personalities.com/personality-types', @doc, @document)
+scrapping.web_scraper
+
+scrapping_articles = Scrap.new('https://www.16personalities.com/articles?category=all', @doc, @document)
+scrapping_articles.web_scraper_articles
